366894ff0917cc88e82c2aa165ea3cc620085fd2
Radoslaw <radek95gg@gmail.com>
2017-11-12 11:42:12 +0100

    Częściowa implementacja bazy danych, dodanie wyświetlania pól w profilu, pobieranie lokalizacji użytkownika i jej zapisywanie oraz niedokończone wyświetlanie lokalicaji w aktywności Map.

--- app/src/main/java/com/example/radoslaw/chatapp/Database.java
+++ app/src/main/java/com/example/radoslaw/chatapp/Database.java
@@ -4,12 +4,21 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.sql.Struct;
 import java.util.HashMap;
@@ -26,10 +35,82 @@ public class Database { //TODO: Pobierać informacje raz, zapisywać do zmiennyc
 
     public static Map<String,Integer> licz = new HashMap<>();
 
+    private static Map<String,Object> userProfile = new HashMap<>();
 
+    private static String userDocumentID;
+    public static GeoPoint loc;
+
+    //Zmienne opisujące dane z rejestracji
     private static String userDisplayName;
     private static String userEmail;
+    private static String useruid;
     private static Uri userPhoto;
+
+
+
+    public static void testPushUser(){
+        userProfile.put("name","Radosław");
+        userProfile.put("surname","Kostek");
+        userProfile.put("sex","Male");
+        userProfile.put("birth",1995);
+        userProfile.put("nationality","Polish");
+        userProfile.put("phoneNumber",694177999);
+        userProfile.put("mail","radek95gg@gmail.com");
+        userProfile.put("organizer","Sprzykówka");
+        userProfile.put("accomodation","All Inlusive");
+    }
+
+    public static void pushUser(){
+        testPushUser();
+        db.collection("users").add(userProfile).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+    }
+
+    public static void assignUser(){
+        db.collection("users")
+                .whereEqualTo("mail",userEmail)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                userDocumentID = document.getId();
+                                userProfile.putAll(document.getData());
+                                assignUserUID();
+                                getlocation(loc);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+
+                    }
+                });
+    }
+
+    public static void assignUserUID(){
+        Map<String, Object> data = new HashMap<>();
+        data.put("uid", useruid);
+        db.collection("users").document(userDocumentID).set(data, SetOptions.merge());
+    }
+
+    public static void getlocation(GeoPoint a){
+        Map<String, GeoPoint> data = new HashMap<>();
+        data.put("location", a);
+        Log.d(TAG, "location =====>" + a);
+        db.collection("users").document(userDocumentID).set(data, SetOptions.merge());
+    }
 
     public static void get(){
         licz.put("jeden",1);
@@ -76,6 +157,7 @@ public class Database { //TODO: Pobierać informacje raz, zapisywać do zmiennyc
         if (user != null) {
             userDisplayName = user.getDisplayName();
             userEmail = user.getEmail();
+            useruid = user.getUid();
             userPhoto = user.getPhotoUrl();
         }
     }
@@ -87,6 +169,10 @@ public class Database { //TODO: Pobierać informacje raz, zapisywać do zmiennyc
 
     public static void setUserDisplayName(String userDisplayName) {
         Database.userDisplayName = userDisplayName;
+    }
+
+    public static Map<String, Object> getUserProfile() {
+        return userProfile;
     }
 
     public static String getUserEmail() {
@@ -104,4 +190,6 @@ public class Database { //TODO: Pobierać informacje raz, zapisywać do zmiennyc
     public static void setUserPhoto(Uri userPhoto) {
         Database.userPhoto = userPhoto;
     }
+
+
 }
